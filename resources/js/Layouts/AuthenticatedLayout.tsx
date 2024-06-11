8daa15b3faf316d5373a202a075a60d7c4ccc33f
Damien Mauchamp <damienmauchamp@gmail.com>
2024-06-10 20:45:40 -0400

    assets, images, favicon, manifest

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,5 +1,4 @@
 import { useState, PropsWithChildren, ReactNode } from "react";
-import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
@@ -23,7 +22,10 @@ export default function Authenticated({
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                                    <img
+                                        src="/assets/hartprint-white_x500.png"
+                                        className="h-9"
+                                    />
                                 </Link>
                             </div>
 
